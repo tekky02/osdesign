34219bd6c1f0a8bbfd016fa2ae254140c286c52a
tekky02 <1657961253@qq.com>
2021-02-04 15:25:43 +0800

    format code

--- src/copier/copier.c
+++ src/copier/copier.c
@@ -6,41 +6,89 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "copier/copier.h"
 
-static bool flag = false;
 static int src_name_size = 0;
 
-void report_error(const char *argv[], int argc, error_type type) {
-  switch (type) {
-  case TOO_FEW_ARGS:
-    fprintf(stderr, "too few arguments provided, please run 'bin/copier "
-                    "--help' for help\n");
+static bool is_directory(const char *file) {
+  struct stat st;
+  if (stat(file, &st) == -1) {
+    perror(file);
+    exit(EXIT_FAILURE);
+  }
+  return S_ISDIR(st.st_mode);
+}
+
+static void show_tip(const char *executable) {
+  fprintf(stderr, "Try '%s --help' for more information.\n", executable);
+}
+
+static void print_help(const char *executable) {
+  printf("Usage: %s [option]... source dest.\n", executable);
+  printf("   or: %s [option]... source... directory.\n", executable);
+  printf("Copy source to dest, or multiple sources to directory.\n");
+  exit(EXIT_SUCCESS);
+}
+
+void arg_check(int argc, const char *argv[]) {
+  switch (argc) {
+  case 1:
+    fprintf(stderr, "%s: missing file operand.\n", argv[0]);
+    show_tip(argv[0]);
+    exit(EXIT_FAILURE);
+  case 2:
+    if (argv[1][0] == '-') {
+      if (strcmp("--help", argv[1]) == 0) {
+        print_help(argv[0]);
+      } else {
+        fprintf(stderr, "%s: invalid option '%s'.\n", argv[0], argv[1]);
+        exit(EXIT_FAILURE);
+      }
+    } else {
+      fprintf(stderr, "%s: missing destination after %s.\n", argv[0], argv[1]);
+      show_tip(argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  case 3:
+    copy(argv[1], argv[2]);
     break;
-  case WRONG_DEST:
-    break;
+  default: // copy source... directory
+           // make sure argv[argc-1] is a directory
+    if (is_directory(argv[argc - 1])) {
+      // for source... copy(source, dest)
+      for (int i = 1; i < argc - 1; ++i) {
+        copy(argv[i], argv[argc - 1]);
+      }
+    } else {
+      fprintf(stderr, "%s: target '%s' is not a directory.\n", argv[argc - 1]);
+      exit(EXIT_FAILURE);
+    }
   }
 }
 
+void run_process(int argc, const char *argv[]) { arg_check(argc, argv); }
+
 void prepare(const char *src, const char *dest) {
+  src_name_size = 0;
   struct stat st;
   char prefix[PATH_MAX];
   if (stat(src, &st) == -1) {
     perror(src);
+    exit(EXIT_FAILURE);
   } else if (S_ISDIR(st.st_mode)) {
     src_name_size = strlen(src) + 1;
     struct stat dst;
     if (stat(dest, &dst) == -1) {
       snprintf(prefix, PATH_MAX, "%s", dest);
     } else if (S_ISDIR(dst.st_mode)) {
-      flag = true;
       snprintf(prefix, PATH_MAX, "%s/%s", dest, src);
     } else {
       fprintf(stderr, "can not overwrite %s\n", dest);
-      exit(1);
+      exit(EXIT_FAILURE);
     }
     mkdir(prefix, st.st_mode);
   }
@@ -51,9 +99,17 @@ void copy(const char *src, const char *dest) { prepare(src, dest); }
 
 void copy_content(int srcfd, int destfd) {
   char buffer[BUFSIZE];
-  size_t n;
+  size_t n = 0;
   while ((n = read(srcfd, buffer, BUFSIZE)) > 0) {
     write(destfd, buffer, n);
+  }
+}
+
+void copy_wrapper(const char *src, const char *dest) {
+  if (is_directory(src)) {
+    copy_directory(src, dest);
+  } else {
+    copy_regular_file();
   }
 }
 
@@ -61,31 +117,26 @@ void open_dest(const char *prefix, const char *src) {
   DIR *directory = opendir(src);
   struct dirent *file_entry = readdir(directory);
   while ((file_entry = readdir(directory)) != NULL) {
-    if (file_entry->d_name[0] == '.')
+    if (file_entry->d_name[0] == '.') {
       continue;
-    if (1) {
-      printf("prefix: %s\n", prefix);
-      char src_file[PATH_MAX];
-      char dest_file[PATH_MAX];
-      snprintf(src_file, PATH_MAX, "%s/%s", src, file_entry->d_name);
-      snprintf(dest_file, PATH_MAX, "%s/%s", prefix, src_file + src_name_size);
-      printf("src: %s\n", src_file);
-      printf("dest: %s\n", dest_file);
-      struct stat st;
-      stat(src_file, &st);
-      if (file_entry->d_type == DT_DIR) {
-        mkdir(dest_file, st.st_mode);
-        open_dest(prefix, src_file);
-      } else if (file_entry->d_type == DT_REG) {
-        int srcfd = open(src_file, O_RDONLY);
-        int destfd = open(dest_file, O_CREAT | O_RDWR | O_TRUNC, st.st_mode);
-        copy_content(srcfd, destfd);
-        close(srcfd);
-        close(destfd);
-      }
+    }
+    char src_file[PATH_MAX];
+    char dest_file[PATH_MAX];
+    snprintf(src_file, PATH_MAX, "%s/%s", src, file_entry->d_name);
+    snprintf(dest_file, PATH_MAX, "%s/%s", prefix, src_file + src_name_size);
+    struct stat st;
+    stat(src_file, &st);
+    if (file_entry->d_type == DT_DIR) {
+      mkdir(dest_file, st.st_mode);
+      open_dest(prefix, src_file);
+    } else if (file_entry->d_type == DT_REG) {
+      int srcfd = open(src_file, O_RDONLY | O_CLOEXEC);
+      int destfd =
+          open(dest_file, O_CREAT | O_RDWR | O_TRUNC | O_CLOEXEC, st.st_mode);
+      copy_content(srcfd, destfd);
+      close(srcfd);
+      close(destfd);
     }
   }
   closedir(directory);
 }
-
-void print_help() {}
